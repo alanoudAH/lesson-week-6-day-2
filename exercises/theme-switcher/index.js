b9434d9d17c762e9b27fa3a3ab241be554f50260
alanoudAH <46870433+alanoudAH@users.noreply.github.com>
2019-03-05 18:39:05 +0300

    HW W6D2

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,34 @@
 // click event for gray button
 
 // click event for white button
+/*
+document.getElementById('grayButton').onclick = switchGray;
+document.getElementById('whiteButton').onclick = switchWhite;
 
 function switchGray() {
-    // change background to gray
-    // change font to white
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'gray';
+document.getElementsByTagName('body')[0].style.color = 'white';
+}
+function switchWhite() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'white';
+document.getElementsByTagName('body')[0].style.color = 'black';
+}*/
+
+
+
+
+function switchGray(){
+
+$("body").css("background-color","gray");
+$("body").css("color","white");
+$("h1").css("color","pink");
 }
 
 function switchWhite() {
-    // change background to white
-    // change font to black
+
+  $("body").css("background-color","white");
+  $("body").css("color","black");
+  
 }
+$("#grayButton").on('click', switchGray);
+$("#whiteButton").on('click', switchWhite);
